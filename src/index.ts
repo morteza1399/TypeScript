aa405bf09bffde2b3a795a38e2b35c7d7f40c559
Morteza Abdollahi <m.helan71@gmail.com>
2024-07-03 21:29:50 +0330

    Learn union type

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
+// union type => A union type is a type formed from two or more other types, representing values that may be any one of those types. 
+
+function kgToLbs(weight: number | string): number {
+  if (typeof weight === 'number')
+    return weight * 2.2
+  else
+    return parseInt(weight) * 2.2
+}
+
+kgToLbs(10);
+kgToLbs("10");
+
 // aliases => variables name must be PascallCase
-type Employee = {
-  readonly id: number;
-  name: string;
-  retrie: (date: Date) => void;
-};
+// type Employee = {
+//   readonly id: number;
+//   name: string;
+//   retrie: (date: Date) => void;
+// };
 
-let employee: Employee = {
-  id: 1,
-  name: "Morteza",
-  retrie: (date: Date) => {
-    console.log(date);
-  },
-};
+// let employee: Employee = {
+//   id: 1,
+//   name: "Morteza",
+//   retrie: (date: Date) => {
+//     console.log(date);
+//   },
+// };
 
-let date = new Date();
-employee.retrie(date);
+// let date = new Date();
+// employee.retrie(date);
