f81a7fe3d950a4784e83d429925f9531ec49f4c5
Morteza Abdollahi <m.helan71@gmail.com>
2024-07-16 16:01:37 +0330

    Remove dist directory

--- dist/tax.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateTax = calculateTax;
-function calculateTax(income) {
-    return income * 0.3;
-}
-//# sourceMappingURL=tax.js.map
